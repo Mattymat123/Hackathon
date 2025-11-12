1aae89aa31d3dfdd64bf0bdf6664c27fd2396b82
cmclausen <cmc@chem.ku.dk>
2025-11-12 13:43:21 +0100

    add reserach

---
+++ ASchat/subagents/researcher/agent.py
@@ -0,0 +1,14 @@
+from google.adk import Agent
+from google.adk.tools import google_search
+
+from . import prompt
+
+MODEL = "gemini-2.5-pro"
+
+researcher_agent = Agent(
+    model=MODEL,
+    name="researcher_agent",
+    instruction=prompt.RESEARCHER_PROMPT,
+    output_key="",
+    tools=[google_search],
+)
---
+++ ASchat/subagents/researcher/prompt.py
@@ -0,0 +1,7 @@
+RESEARCHER_PROMPT = """
+Du er en research agent specialiseret i at finde den information som embedsmænd i den danske 
+statforvaltning har brug at skrive præcise og omfattende aktstykker. Din rolle er at udføre dybdegående research
+for at indsamle relevante oplysninger, data og kilder, og præsentere dem i punktform til den næste agent i kæden, som skal skrive aktstykket.
+Lig derfor vægt på at få al den nødvendige information med og prioriter at få alle detaljer med i et struktureret format uden at ligge
+ vægt på læsbarhed, da næste agent vil omformulere og strukturere informationen.
+"""
