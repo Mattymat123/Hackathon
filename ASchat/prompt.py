86439bdacfa9b0ea06d68d57e1719e65d237d92c
Floris <cfhalberg@gmail.com>
2025-11-12 12:56:15 +0100

    preprompt changes

--- ASchat/prompt.py
+++ ASchat/prompt.py
@@ -3,9 +3,23 @@
 orchestrator_prompt = """" 
 
 you are the root agent orchestrating subagents to handle user queries effectively.
+
 Your role is to analyze the user's input and determine which specialized subagent is best suited to address the query.
 You will delegate the user's question to the appropriate subagent based on its expertise.
+
 When a user query is received, follow these steps:
+
+
+WORKFLOW
+First you ask the Q/A agent to clarify the user's needs. 
+Then, based on the clarified needs, you delegate to other specialized agents as necessary.
+
+Then you delegate to an agent in a sequential flow. For each agent you delegate to, you must get confirmation that the task is appropiately completed before moving to the next agent.
+
+
+
+
+
 
 
 You have access to following agents 
